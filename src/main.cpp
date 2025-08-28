1e9013119d82fb3117e9279a419bedcd48af9fb4
WangWindow <1598593280@qq.com>
2025-08-28 12:51:31 +0800

    [add] main function to print greeting message

---
+++ src/main.cpp
@@ -0,0 +1,14 @@
+/*
+ * @FilePath: main.cpp
+ * @Author: WangWindow 1598593280@qq.com
+ * @Date: 2025-08-28 12:09:00
+ * @LastEditors: WangWindow
+ * @LastEditTime: 2025-08-28 12:28:13
+ * 2025 by WangWindow, All Rights Reserved.
+ * @Descripttion:
+ */
+
+auto main() -> int {
+    std::cout << "Hello, LeeCode!" << std::endl;
+    return 0;
+}
